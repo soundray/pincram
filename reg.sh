39797dfbfe3d80f183cad1b74a7049ade463155d
Rolf Heckemann <git@soundray.de>
2018-09-12 12:08:04 +0000

    Undo set -vx commands (reg.sh)

--- reg.sh
+++ reg.sh
@@ -41,7 +41,7 @@ esac
 td=$(tempdir)
 trap finish EXIT
 cd $td
-set -vx
+
 if [[ $idx -gt 0 ]] 
 then
     split -a 4 -l $chunkn -d $wd/job.conf
@@ -54,7 +54,7 @@ fi
 
 cat thischunk | sort -R | while read params
 do
-set +vx
+
     (( loopc += 1 ))
 
     set -- $(echo $params)
