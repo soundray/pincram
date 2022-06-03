4e44720ef3440bf140525ec480fa044bcd108304
Rolf A. Heckemann <git@soundray.de>
2022-06-03 12:18:23 +0200

    pincram.sh: fix indentation

--- pincram.sh
+++ pincram.sh
@@ -5,7 +5,7 @@ set -e
 ### Usage & parameter handling
 
 usage () {
-cat <<EOF
+    cat <<EOF
 
 Copyright (C) 2012-2018 Rolf A. Heckemann
 Web site: http://www.soundray.org/pincram
@@ -21,17 +21,17 @@ Usage: $0 <input> <options> <-result result-dir/> \\
               will be overwritten if they exist.
 
 -workdir    : Base working directory. Default is present working directory. When running under PBS, this
-              location needs to be accessible from the cluster nodes. On each run, a uniquely named directory 
+              location needs to be accessible from the cluster nodes. On each run, a uniquely named directory
               for intermediate results is generated.
 
--pickup     : Intermediate results directory from a previous run -- work will be continued. Overrides 
+-pickup     : Intermediate results directory from a previous run -- work will be continued. Overrides
               -workdir setting if given. Implies -savewd. Previous run must be compatible (same -atlas,
               same <input>, etc.), else results are unpredictable.
 
--savewd     : By default, the temporary directory under the working directory will be deleted 
+-savewd     : By default, the temporary directory under the working directory will be deleted
               after processing. Set this flag to save intermediate files in the -result location.
 
--savedm     : By default, the final distance map is discarded. Set this flag to save it to the result 
+-savedm     : By default, the final distance map is discarded. Set this flag to save it to the result
               directory instead
 
 -atlas      : Atlas directory.
@@ -73,11 +73,11 @@ commandline="$pn $*"
 
 case $PINCRAM_USE_LIB in
     irtk)
-	type areg2 || fatal "Missing binary: areg2 (IRTK) not on path" ;;
+        type areg2 || fatal "Missing binary: areg2 (IRTK) not on path" ;;
     mirtk)
-	type mirtk || fatal "Missing binary: mirtk not on path" ;;
+        type mirtk || fatal "Missing binary: mirtk not on path" ;;
     greedy)
-	type greedy || fatal "Missing binary: greedy not on path" ;;
+        type greedy || fatal "Missing binary: greedy not on path" ;;
 esac
 
 export PINCRAM_ARCH PINCRAM_USE_LIB
@@ -102,27 +102,27 @@ pickup=
 while [[ $# -gt 0 ]]
 do
     case "$1" in
-	-tpn)               tpn=$(realpath "$2"); shift;;
-	-result)         result=$(realpath "$2"); shift;;
-	-atlas)           atlas=$(realpath "$2"); shift;;
-	-workdir)       workdir=$(realpath "$2"); shift;;
-	-pickup)         pickup=$(realpath "$2"); shift;;
-	-ref)               ref=$(realpath "$2"); shift;;
-	-savewd)         savewd=1 ;;
-	-savedm)         savedm=1 ;;
-	-atlasn)         atlasn="$2"; shift;;
-	-levels)         levels="$2"; shift;;
-	-par)               par="$2"; shift;;
-	--) shift; break;;
+        -tpn)               tpn=$(realpath "$2"); shift;;
+        -result)         result=$(realpath "$2"); shift;;
+        -atlas)           atlas=$(realpath "$2"); shift;;
+        -workdir)       workdir=$(realpath "$2"); shift;;
+        -pickup)         pickup=$(realpath "$2"); shift;;
+        -ref)               ref=$(realpath "$2"); shift;;
+        -savewd)         savewd=1 ;;
+        -savedm)         savedm=1 ;;
+        -atlasn)         atlasn="$2"; shift;;
+        -levels)         levels="$2"; shift;;
+        -par)               par="$2"; shift;;
+        --) shift; break;;
         -*)
             fatal "Unknown parameter" ;;
-	*)  break;;
+        *)  break;;
     esac
     shift
 done
 
 [[ -n "$result" ]] || fatal "Result directory name not set (e.g. -result pincram-masks)"
-mkdir -p "$result" 
+mkdir -p "$result"
 [[ -d "$result" ]] || fatal "Failed to create directory for result output ($result)"
 
 [[ -e "$atlas" ]] || fatal "Atlas directory or file does not exist"
@@ -137,9 +137,9 @@ msg "$(date)"
 msg "Extracting $tgt"
 msg "Writing brain label to $result"
 
-if [[ -n $PINCRAM_PROCEED_PCT ]] 
-then 
-    minpct=$PINCRAM_PROCEED_PCT 
+if [[ -n $PINCRAM_PROCEED_PCT ]]
+then
+    minpct=$PINCRAM_PROCEED_PCT
 else
     minpct=100
 fi
@@ -148,10 +148,10 @@ fi
 
 finish () {
     if [[ $savewd -eq 1 ]] ; then
-	chmod -R u+rwX $td
-	mv "$td" "$result"
+        chmod -R u+rwX $td
+        mv "$td" "$result"
     else
-	rm -rf "$td"
+        rm -rf "$td"
     fi
     exit
 }
@@ -165,8 +165,8 @@ labelstats() {
 assess() {
     local glabels="$1"
     if [[ -e ref.nii.gz ]] ; then
-	mirtk edit-image "$glabels" assess.nii.gz -copy-size ref.nii.gz >>noisy.log 2>&1
-	echo -e "${glabels}:\t\t"$(labelstats ref.nii.gz assess.nii.gz -false)
+        mirtk edit-image "$glabels" assess.nii.gz -copy-size ref.nii.gz >>noisy.log 2>&1
+        echo -e "${glabels}:\t\t"$(labelstats ref.nii.gz assess.nii.gz -false)
     fi
     return 0
 }
@@ -179,19 +179,19 @@ origin() {
 nmi() {
     local img=$1
     mirtk evaluate-similarity \
-	target-full.nii.gz $img \
-	-mask emargin-$thislevel-dil.nii.gz \
-	-metric NMI \
-	-precision 7 \
-	-table \
-	-threads 1 \
-	-noid | tail -n 1
+          target-full.nii.gz $img \
+          -mask emargin-$thislevel-dil.nii.gz \
+          -metric NMI \
+          -precision 7 \
+          -table \
+          -threads 1 \
+          -noid | tail -n 1
 }
 
 odistmap() {
     local img=$1 ; shift
     local out=$1
-    seg_maths $img -smo 6 -otsu im-otsu.nii.gz 
+    seg_maths $img -smo 6 -otsu im-otsu.nii.gz
     mirtk calculate-distance-map im-otsu.nii.gz odm.nii.gz -threads $par
     mirtk calculate-element-wise odm.nii.gz -mul -1 -threads $par -o $out
 }
@@ -202,12 +202,12 @@ eucmap() {
     mirtk calculate-distance-map $mask dm.nii.gz -threads $par
     mirtk calculate-element-wise dm.nii.gz -mul -1 -threads $par -o $map
 }
- 
+
 ### Core working directory
 
-if [[ -n "$pickup" ]] 
+if [[ -n "$pickup" ]]
 then
-    [[ -d $pickup ]] || fatal "Pickup directory $pickup does not exist" 
+    [[ -d $pickup ]] || fatal "Pickup directory $pickup does not exist"
     td=$pickup
     savewd=1
     cd "$td" || fatal "Error: cannot cd to temp directory $td"
@@ -215,20 +215,20 @@ then
     touch 0.tar
     set -- *.tar
     shift
-    while [[ $# -gt 0 ]] 
+    while [[ $# -gt 0 ]]
     do
-	tar -xf $1 ; rm $1 ; shift
+        tar -xf $1 ; rm $1 ; shift
     done
     touch 0.log ; rm *.log
     touch weights0.csv ; rm weights*.csv
 else
     if [[ $PINCRAM_ARCH == "pbs" ]] ; then
-	mkdir -p "$workdir"
-	td=$(mktemp -d "$workdir/$(basename $0).XXXXXX") || fatal "Could not create working directory in $workdir"
+        mkdir -p "$workdir"
+        td=$(mktemp -d "$workdir/$(basename $0).XXXXXX") || fatal "Could not create working directory in $workdir"
     else
-	: ${TMPDIR:=/tmp/$USER}
-	mkdir -p $TMPDIR
-	td=$(mktemp -d $TMPDIR/$(basename $0).XXXXXX) || fatal "Could not create working directory in $TMPDIR"
+        : ${TMPDIR:=/tmp/$USER}
+        mkdir -p $TMPDIR
+        td=$(mktemp -d $TMPDIR/$(basename $0).XXXXXX) || fatal "Could not create working directory in $TMPDIR"
     fi
 fi
 export PINCRAM_WORKDIR=$td
@@ -241,10 +241,10 @@ msg "Working in directory $td"
 
 if [[ -d "$atlas" ]] ; then
     if [[ -e "$atlas"/etc/atlases.csv ]] ; then
-	atlas="$atlas"/etc/atlases.csv
+        atlas="$atlas"/etc/atlases.csv
     else
-	"$cdir"/atlas-csv-gen.sh "$atlas" atlases.csv
-	atlas=$PWD/atlases.csv
+        "$cdir"/atlas-csv-gen.sh "$atlas" atlases.csv
+        atlas=$PWD/atlases.csv
     fi
 fi
 
@@ -272,28 +272,28 @@ echo "$commandline" >commandline.log
 ### Target preparation
 
 originalorigin=$(origin "$tgt")
-if [[ -z $pickup ]] 
-then 
+if [[ -z $pickup ]]
+then
     mirtk edit-image "$tgt" target-full.nii.gz -origin 0 0 0
     mirtk convert-image target-full.nii.gz target-full.nii.gz -float
     [[ -e "$ref" ]] && mirtk edit-image "$ref" ref.nii.gz -origin 0 0 0 && chmod +w ref.nii.gz
     if [[ -n $refspace ]] ; then
-	msg "Calculating affine normalization to reference space with distance maps"
-	odistmap $refspace refspace-dm.nii.gz 
-	odistmap target-full.nii.gz target-dm.nii.gz 
-	mirtk register refspace-dm.nii.gz target-dm.nii.gz \
-	      -model Affine \
-	      -sim SSD \
-	      -dofout pre-dof.gz \
-	      -level 4 \
-	      -threads $par >noisy.log 2>&1
-	mirtk register $refspace target-full.nii.gz \
-	      -model Affine \
-	      -dofin pre-dof.gz \
-	      -dofout tpn.dof.gz \
-	      -levels 3 1 \
-	      -threads $par >noisy.log 2>&1
-	tpn=$td/tpn.dof.gz
+        msg "Calculating affine normalization to reference space with distance maps"
+        odistmap $refspace refspace-dm.nii.gz
+        odistmap target-full.nii.gz target-dm.nii.gz
+        mirtk register refspace-dm.nii.gz target-dm.nii.gz \
+              -model Affine \
+              -sim SSD \
+              -dofout pre-dof.gz \
+              -level 4 \
+              -threads $par >noisy.log 2>&1
+        mirtk register $refspace target-full.nii.gz \
+              -model Affine \
+              -dofin pre-dof.gz \
+              -dofout tpn.dof.gz \
+              -levels 3 1 \
+              -threads $par >noisy.log 2>&1
+        tpn=$td/tpn.dof.gz
     fi
 fi
 
@@ -323,76 +323,76 @@ for level in $(seq 0 $maxlevel) ; do
     thislevel=${levelname[$level]}
     msg "Level $thislevel"
     cat /dev/null >job.conf
-    
+
     ## Prep datasets line by line in job.conf
     for srcindex in $(cat selection-$prevlevel.csv) ; do
 
-	# Read in atlas
-	atlasname= ; src= ; mrgorspn= ; mrg= ; spn= ; msk= ; alt= ; mrggen= 
-	set -- $(head -n $[$srcindex+1] $atlas | tail -n 1 | tr ',' ' ')
-	atlasname=$1 ; shift
-	src=$atlasbase/$1 ; shift
-	spn=$atlasbase/$1 ; shift
-	msk=$atlasbase/$1 ; shift
-	alt=$atlasbase/$1 ; shift
-	if [[ $level -ge 2 ]] ; then 
-	    mrggen=$td/mrggen-s$srcindex.nii.gz
-	    if [[ ! -e $mrggen ]] ; then
-		mrg=$td/mrg-s$srcindex.nii.gz
-		seg_maths $msk -abs -uthr 7 -bin -mul $src $mrggen
-		src=$mrggen
-	    fi
-	fi
-	srctr="$PWD"/srctr-$thislevel-s$srcindex.nii.gz
-	masktr="$PWD"/masktr-$thislevel-s$srcindex.nii.gz
-	dofin="$PWD"/reg-s$srcindex-$prevlevel.dof.gz
-	dofout="$PWD"/reg-s$srcindex-$thislevel.dof.gz
-	alttr="$PWD"/alttr-$thislevel-s$srcindex.nii.gz
+        # Read in atlas
+        atlasname= ; src= ; mrgorspn= ; mrg= ; spn= ; msk= ; alt= ; mrggen=
+        set -- $(head -n $[$srcindex+1] $atlas | tail -n 1 | tr ',' ' ')
+        atlasname=$1 ; shift
+        src=$atlasbase/$1 ; shift
+        spn=$atlasbase/$1 ; shift
+        msk=$atlasbase/$1 ; shift
+        alt=$atlasbase/$1 ; shift
+        if [[ $level -ge 2 ]] ; then
+            mrggen=$td/mrggen-s$srcindex.nii.gz
+            if [[ ! -e $mrggen ]] ; then
+                mrg=$td/mrg-s$srcindex.nii.gz
+                seg_maths $msk -abs -uthr 7 -bin -mul $src $mrggen
+                src=$mrggen
+            fi
+        fi
+        srctr="$PWD"/srctr-$thislevel-s$srcindex.nii.gz
+        masktr="$PWD"/masktr-$thislevel-s$srcindex.nii.gz
+        dofin="$PWD"/reg-s$srcindex-$prevlevel.dof.gz
+        dofout="$PWD"/reg-s$srcindex-$thislevel.dof.gz
+        alttr="$PWD"/alttr-$thislevel-s$srcindex.nii.gz
 
-	if [[ ! -s $masktr ]]
-	then
-	    echo "-tgt $tgt" "-tdm $tdm" "-src $src" "-srctr $srctr" "-msk $msk" "-masktr $masktr" "-alt $alt" "-alttr $alttr" "-dofin $dofin" "-dofout $dofout" "-spn $spn" "-tpn $tpn" "-lev $level" "-tmargin $tmg" "-par $par" >>$td/job.conf
-	fi
+        if [[ ! -s $masktr ]]
+        then
+            echo "-tgt $tgt" "-tdm $tdm" "-src $src" "-srctr $srctr" "-msk $msk" "-masktr $masktr" "-alt $alt" "-alttr $alttr" "-dofin $dofin" "-dofout $dofout" "-spn $spn" "-tpn $tpn" "-lev $level" "-tmargin $tmg" "-par $par" >>$td/job.conf
+        fi
     done
 
 
     ## Launch parallel registrations
     cp job.conf job-$thislevel.conf
-    if [[ -s job.conf ]] 
+    if [[ -s job.conf ]]
     then
-	msg "Launching registrations"
-	csec=$("$cdir"/distrib -script "$cdir"/reg.sh -datalist $td/job.conf -level $level -jobs $par)
-	etasec=$(( $(date +%s) + $csec ))
-	eta=$(date -d "@$etasec")
-	[[ $PINCRAM_ARCH == "pbs" ]] && msg "First job status check at $eta" 
+        msg "Launching registrations"
+        csec=$("$cdir"/distrib -script "$cdir"/reg.sh -datalist $td/job.conf -level $level -jobs $par)
+        etasec=$(( $(date +%s) + $csec ))
+        eta=$(date -d "@$etasec")
+        [[ $PINCRAM_ARCH == "pbs" ]] && msg "First job status check at $eta"
     fi
 
 
     ## Monitor incoming results and wait
     loopcount=0
     masksready=0
-    minready=$[$nselected*$minpct/100] 
+    minready=$[$nselected*$minpct/100]
     echo -n $($cdir/spark 0 $masksready $nselected | cut -c 2)
     sleeptime=$[$level*5+5]
     until [[ $masksready -ge $minready ]]
     do
-	(( loopcount += 1 ))
-	[[ loopcount -gt 500 ]] && fatal "Waited too long for registration results"
-	prevmasksready=$masksready
-	masksready=$( ls masktr-$thislevel-s* 2>/dev/null | wc -l )
-	[[ $masksready -gt $prevmasksready ]] && loopcount=0
-	[[ $masksready -eq 1 ]] && masksready=0
-	echo -n $($cdir/spark 0 $masksready $nselected | cut -c 2)
-	if [[ $(date +%s ) -gt $etasec ]] 
-	then
-	    echo
-	    fatal "Masks not ready by deadline. Relaunching registrations" # TODO: change back to "msg"
-	    csec=$("$cdir"/distrib -script "$cdir"/reg.sh -datalist $td/job.conf -level $level -jobs $par)
-	    etasec=$(( $(date +%s) + $csec ))
-	    eta=$(date -d "@$etasec")
-	    [[ $PINCRAM_ARCH == "pbs" ]] && msg "Next job status check at $eta" 
-	fi
-	sleep $sleeptime
+        (( loopcount += 1 ))
+        [[ loopcount -gt 500 ]] && fatal "Waited too long for registration results"
+        prevmasksready=$masksready
+        masksready=$( ls masktr-$thislevel-s* 2>/dev/null | wc -l )
+        [[ $masksready -gt $prevmasksready ]] && loopcount=0
+        [[ $masksready -eq 1 ]] && masksready=0
+        echo -n $($cdir/spark 0 $masksready $nselected | cut -c 2)
+        if [[ $(date +%s ) -gt $etasec ]]
+        then
+            echo
+            fatal "Masks not ready by deadline. Relaunching registrations" # TODO: change back to "msg"
+            csec=$("$cdir"/distrib -script "$cdir"/reg.sh -datalist $td/job.conf -level $level -jobs $par)
+            etasec=$(( $(date +%s) + $csec ))
+            eta=$(date -d "@$etasec")
+            [[ $PINCRAM_ARCH == "pbs" ]] && msg "Next job status check at $eta"
+        fi
+        sleep $sleeptime
     done
     echo
     msg "Minimum number of mask transformations calculated"
@@ -424,11 +424,11 @@ for level in $(seq 0 $maxlevel) ; do
     set -- $(cat selection-$prevlevel.csv)
     set -- $(for i ; do ls srctr-$thislevel-s$i.nii.gz ; done)
     mirtk evaluate-similarity target-full.nii.gz $@ \
-			-mask emargin-$thislevel-dil.nii.gz \
-			-metric NMI -precision 7 -threads $par \
-			-table -header off |\
-	rev | cut -d s -f 1 | rev | sort -rn -t , -k 2 | tee simm-$thislevel.csv |\
-	cut -d , -f 1 >ranking-$thislevel.csv
+          -mask emargin-$thislevel-dil.nii.gz \
+          -metric NMI -precision 7 -threads $par \
+          -table -header off |\
+        rev | cut -d s -f 1 | rev | sort -rn -t , -k 2 | tee simm-$thislevel.csv |\
+        cut -d , -f 1 >ranking-$thislevel.csv
 
     tar -cf srctr-$thislevel.tar srctr-$thislevel-s*.nii.gz ; rm srctr-$thislevel-s*.nii.gz
     tar -cf alttr-$thislevel.tar alttr-$thislevel-s*.nii.gz
@@ -462,7 +462,7 @@ for level in $(seq 0 $maxlevel) ; do
     seg_maths tmask-$thislevel-sel-sum.nii.gz -div $scalefactor distmap-$thislevel.nii.gz
     [[ $level -eq $maxlevel ]] && continue
     thresh2=$( echo "( $level - 6 )^2 / 5" | bc -l )
-    seg_maths distmap-$thislevel.nii.gz -abs -uthr $thresh2 -bin dmargin-$thislevel.nii.gz 
+    seg_maths distmap-$thislevel.nii.gz -abs -uthr $thresh2 -bin dmargin-$thislevel.nii.gz
     tmg="$PWD"/dmargin-$thislevel.nii.gz
 done
 
